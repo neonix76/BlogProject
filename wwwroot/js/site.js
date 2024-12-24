addb2f1d54d9d4f791e868b9288e8ac4f701875b
neonix76 <sergeu12890@yandex.ru>
2024-12-25 02:17:14 +0300

    че?

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Получаем модальное окно
+var modal = document.getElementById("myModal");
 
-// Write your JavaScript code.
+// Получаем кнопку, которая открывает модальное окно
+var btn = document.getElementById("btnOpenModal");
+
+// Получаем элемент <span>, который закрывает модальное окно
+var span = document.getElementsByClassName("close")[0];
+
+// Когда пользователь нажимает на кнопку, открываем модальное окно
+btn.onclick = function (event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    modal.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закрываем модальное окно
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Когда пользователь нажимает в любом месте вне модального окна, закрываем его
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
